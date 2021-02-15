7bd3ee4e1f7ea8f9f0fddba82cf92902a0019873
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-15 14:26:26 +0100

    fixed autofill problem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,10 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:question]
+    @question = params[:question].strip
     if @question == "I am going to work"
       @answer = "Great!"
-    elsif @question[-1] == "?"
+    elsif @question.ends_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else 
       @answer = "I don't care, get dressed and go to work!"
